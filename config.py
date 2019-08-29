6a7a83484563310c81b196cce1d9eb52bc061b9f
jiangminghao <jiangminghao@antiy.cn>
2019-08-29 11:06:05 +0800

    修改回局域网ip做测试

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import  json
 config = {
         'inbound_port':9901,
         'outbound_port':9900,
-        'outbound_ip':'47.254.71.134',
+        'outbound_ip':'172.16.30.180',
         'outbound_url':'/api/cmiddle/outbound',
         'outbound_next_packet_url':'/api/cmiddle/npacket',
-        'mongodb_ip':'47.254.71.134',
+        'mongodb_ip':'172.16.30.180',
         'mongodb_username':'s9',
         'mongodb_pwd':'123456',
         'mongodb_dbname':'CMiddlebox',
